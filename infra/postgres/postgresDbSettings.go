1facc99898597617e2fffbfdee5d77201a7a49fb
Manuel Pineda <pineda.manuel@gmail.com>
2023-05-18 09:31:05 -0500

    Postgres setting fix

--- infra/postgres/postgresDbSettings.go
+++ infra/postgres/postgresDbSettings.go
@@ -3,5 +3,5 @@ package postgres
 type PostgresDbSettings struct {
 	User     string `envconfig:"POSTGRES_USER" required:"true"`
 	Password string `envconfig:"POSTGRES_PASSWORD" required:"true"`
-	Db       string `envconfig:"POSTGRES_URL" required:"true"`
+	Db       string `envconfig:"POSTGRES_DB" required:"true"`
 }
